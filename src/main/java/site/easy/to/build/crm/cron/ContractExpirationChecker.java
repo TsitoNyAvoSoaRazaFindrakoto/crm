c83bbc008b6c57aa08031f8d1ba2f3ec44407344
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-20 11:13:04 +0300

    Update ContractExpirationChecker.java

--- src/main/java/site/easy/to/build/crm/cron/ContractExpirationChecker.java
+++ src/main/java/site/easy/to/build/crm/cron/ContractExpirationChecker.java
@@ -12,29 +12,29 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class ContractExpirationChecker {
 
-    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+	private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
-    @Scheduled(cron = "0 0 0 * * *")
-    public void scheduleContractExpirationCheck(Contract contract) {
-        Runnable checkExpirationTask = () -> {
-            LocalDate currentDate = LocalDate.now();
-            LocalDate endDate = LocalDate.parse(contract.getEndDate());
-            if (currentDate.isAfter(endDate)) {
-                // Contract has expired
-                contract.setStatus("expired");
+	@Scheduled(cron = "0 0 0 * * *")
+	public void scheduleContractExpirationCheck(Contract contract) {
+		Runnable checkExpirationTask = () -> {
+			LocalDate currentDate = LocalDate.now();
+			LocalDate endDate = LocalDate.parse(contract.getEndDate());
+			if (currentDate.isAfter(endDate)) {
+				// Contract has expired
+				contract.setStatus("expired");
 
-                System.out.println("Contract expired: " + contract.getContractId());
-            } else {
-                // Contract is still valid
-                System.out.println("Contract is still valid: " + contract.getContractId());
-            }
-        };
+				System.out.println("Contract expired: " + contract.getContractId());
+			} else {
+				// Contract is still valid
+				System.out.println("Contract is still valid: " + contract.getContractId());
+			}
+		};
 
-        // Schedule the task to run periodically
-        executorService.scheduleAtFixedRate(checkExpirationTask, 0, 1, TimeUnit.DAYS);
-    }
+		// Schedule the task to run periodically
+		executorService.scheduleAtFixedRate(checkExpirationTask, 0, 1, TimeUnit.DAYS);
+	}
 
-    public void stopContractExpirationCheck() {
-        executorService.shutdown();
-    }
+	public void stopContractExpirationCheck() {
+		executorService.shutdown();
+	}
 }
