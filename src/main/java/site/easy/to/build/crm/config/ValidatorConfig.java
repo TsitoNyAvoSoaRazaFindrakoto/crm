cba426decb36405ff480a01c543fca376236b01e
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-22 19:54:38 +0300

    Create ValidatorConfig.java
    injection de dependances

---
+++ src/main/java/site/easy/to/build/crm/config/ValidatorConfig.java
@@ -0,0 +1,18 @@
+package site.easy.to.build.crm.config;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ValidatorConfig {
+
+  @Bean
+  public Validator validator() {
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    return factory.getValidator();
+  }
+}
+
