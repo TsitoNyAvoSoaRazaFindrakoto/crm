0f25b4f071e09fb3ab8a51da3e65e2697ed2fc87
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-22 11:58:33 +0300

    Create QuickActionsService.java
    service pour l'importation, la generation et la suppression de donnees

---
+++ src/main/java/site/easy/to/build/crm/extension/service/QuickActionsService.java
@@ -0,0 +1,28 @@
+package site.easy.to.build.crm.extension.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import site.easy.to.build.crm.extension.io.DbReset;
+import site.easy.to.build.crm.extension.io.input.DbImport;
+
+@Service
+@AllArgsConstructor
+public class QuickActionsService {
+	private final DbReset dbReset;
+	private final DbImport dbImport;
+
+  public void resetDatabase() {
+    dbReset.resetDatabase();
+  }
+
+  public int generateData() {
+//    TODO database generato
+    return 0;
+  }
+
+	public int importData(MultipartFile file, String clazz) {
+		dbImport.
+    return 0;
+  }
+}
