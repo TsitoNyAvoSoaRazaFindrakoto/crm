cdcaaec3b5eb559c25dad1c4273f1b42009f761c
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-24 15:53:05 +0300

    Update CrmApplicationTests.java

--- src/test/java/site/easy/to/build/crm/CrmApplicationTests.java
+++ src/test/java/site/easy/to/build/crm/CrmApplicationTests.java
@@ -1,30 +1,14 @@
 package site.easy.to.build.crm;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import site.easy.to.build.crm.service.budget.BudgetService;
-import site.easy.to.build.crm.service.budget.ExpenseService;
-import site.easy.to.build.crm.service.customer.CustomerService;
 
-import java.util.List;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CrmApplicationTests {
 
-	@Autowired
-	BudgetService budgetService;
-	@Autowired
-	CustomerService customerService;
-	@Autowired
-	ExpenseService expenseService;
-
 	@Test
 	void contextLoads() {
-		double sumBudget = budgetService.getTotalBudgetByCustomer(43);
-		double sumExpense = expenseService.getTotalExpenses(43);
-		System.out.println("budget "+sumBudget);
-		System.out.println("expense "+sumExpense);
 	}
 
 }
