00fe8682f4ceac219b4ce95bcbd63676be9cb7ca
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-24 10:16:29 +0300

    Update ExpenseRepository.java

--- src/main/java/site/easy/to/build/crm/repository/ExpenseRepository.java
+++ src/main/java/site/easy/to/build/crm/repository/ExpenseRepository.java
@@ -12,41 +12,40 @@ import java.util.List;
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
 
-    @Query(value = """
-                    SELECT
-                        expense.*
-                        FROM trigger_ticket
-                        JOIN expense ON expense.expense_id = trigger_ticket.expense_id
-                    WHERE trigger_ticket.expense_id IS NOT NULL
-            """, nativeQuery = true)
-    List<Expense> findAllByTicketIsNotNull();
+  @Query(value = """
+            SELECT
+                expense.*
+                FROM trigger_ticket
+                JOIN expense ON expense.expense_id = trigger_ticket.expense_id
+            WHERE trigger_ticket.expense_id IS NOT NULL
+    """, nativeQuery = true)
+  List<Expense> findAllByTicketIsNotNull();
 
-    @Query(value = """
-                    SELECT
-                        expense.*
-                        FROM trigger_lead
-                        JOIN expense ON expense.expense_id = trigger_lead.expense_id
-                    WHERE trigger_lead.expense_id IS NOT NULL
-            """, nativeQuery = true)
-    List<Expense> findAllByLeadIsNotNull();
+  @Query(value = """
+            SELECT
+                expense.*
+                FROM trigger_lead
+                JOIN expense ON expense.expense_id = trigger_lead.expense_id
+            WHERE trigger_lead.expense_id IS NOT NULL
+    """, nativeQuery = true)
+  List<Expense> findAllByLeadIsNotNull();
 
 
-    @Query("""
-            SELECT SUM(e.amount)
-            FROM Ticket t
-            JOIN t.expense e
-            WHERE t.customer.customerId = :customerId
-            """)
-    Double sumExpenseTicketByCustomerID(@Param("customerId") Integer customerId);
+  @Query("""
+    SELECT SUM(e.amount)
+    FROM Ticket t
+    JOIN t.expense e
+    WHERE t.customer.customerId = :customerId
+    """)
+  Double sumExpenseTicketByCustomerID(@Param("customerId") Integer customerId);
 
-    @Query("""
-            SELECT SUM(e.amount)
-            FROM Lead l
-            JOIN l.expense e
-            WHERE l.customer.customerId = :customerId
-            """)
-    Double sumExpenseLeadByCustomerID(@Param("customerId") Integer customerId);
-
+  @Query("""
+    SELECT SUM(e.amount)
+    FROM Lead l
+    JOIN l.expense e
+    WHERE l.customer.customerId = :customerId
+    """)
+  Double sumExpenseLeadByCustomerID(@Param("customerId") Integer customerId);
 
 
 }
