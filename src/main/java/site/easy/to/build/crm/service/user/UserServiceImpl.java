cdeb57391907471f2560e45c4d00e4dc870a802c
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-26 02:25:50 +0300

    Update UserServiceImpl.java

--- src/main/java/site/easy/to/build/crm/service/user/UserServiceImpl.java
+++ src/main/java/site/easy/to/build/crm/service/user/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package site.easy.to.build.crm.service.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import site.easy.to.build.crm.repository.UserRepository;
