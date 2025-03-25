53d196e059d579f651220ae5a9087d52d0636e26
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-26 02:12:12 +0300

    dtos for csv

--- src/main/java/site/easy/to/build/crm/extension/util/io/dto/CustomerBudgetCsv.java
+++ src/main/java/site/easy/to/build/crm/extension/util/io/dto/CustomerBudgetCsv.java
@@ -2,29 +2,29 @@ package site.easy.to.build.crm.extension.util.io.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@JsonPropertyOrder({ "customer_email", "customer_name" })
-public class CustomerCsv implements Serializable {
+@JsonPropertyOrder({"customer_email", "Budget"})
+public class CustomerBudgetCsv implements Serializable {
 
   @Email
   @NotBlank
   @JsonProperty("customer_email")
   String customerEmail;
 
-  @NotBlank
-  @JsonProperty("customer_name")
-  String customerName;
-
+  @Positive
+  @JsonProperty("Budget")
+  Double budget;
 }
