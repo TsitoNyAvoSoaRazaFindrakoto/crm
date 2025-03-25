55d4bf48b2e5c5831b5e80a101b442ae9398c69e
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-26 02:11:57 +0300

    Update CsvImport.java
    refactor

--- src/main/java/site/easy/to/build/crm/extension/util/io/CsvImport.java
+++ src/main/java/site/easy/to/build/crm/extension/util/io/CsvImport.java
@@ -3,55 +3,55 @@ package site.easy.to.build.crm.extension.util.io;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 import org.springframework.stereotype.Component;
-import site.easy.to.build.crm.extension.Exception.CsvValidationException;
-import site.easy.to.build.crm.extension.Exception.LocalValidationException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 @Component
 public class CsvImport {
 
   private final CsvMapper csvMapper;
+  private final Validator validator;
 
-  public CsvImport() {
-    this.csvMapper = new CsvMapper();
+  public CsvImport(Validator validator) {
+    this.validator = validator;
+    this.csvMapper = CsvMapper.builder().build();
   }
 
-  public <T, C extends CsvBase<T>> List<C> readToCsv(Class<T> motherEntity, InputStream inputStream, Validator validator) throws IOException, CsvValidationException {
-    @SuppressWarnings("unchecked") Class<C> targetClass = (Class<C>) motherEntity.getAnnotation(CsvDTO.class).value();
-    CsvSchema schema = csvMapper.schemaFor(targetClass).withHeader();
-    MappingIterator<C> mappingIterator = csvMapper.readerFor(targetClass).with(schema).readValues(inputStream);
-    
-    List<C> csvObjects = mappingIterator.readAll();
-    
-    // Validate all objects after reading
-		for (int i = 0; i < csvObjects.size(); i++) {
-      csvObjects.get(i).validate(validator);
+  public <T> List<T> readToCsv(Class<T> targetClass, InputStream inputStream, List<String> errorMessages) {
+    List<T> csvObjects = new ArrayList<>();
+    try {
+      CsvSchema schema = csvMapper.schemaFor(targetClass).withHeader();
+      MappingIterator<T> mappingIterator = csvMapper.readerFor(targetClass).with(schema).readValues(inputStream);
+      csvObjects = mappingIterator.readAll();
+    } catch (IOException e) {
+      errorMessages.add("Error reading CSV: " + e.getMessage());
+      return csvObjects; // Return already read objects, or empty list.
     }
-    
+    validateCsvObjects(csvObjects, errorMessages);
     return csvObjects;
   }
 
-  public List<?> readToEntity(Class<?> targetClass, InputStream inputStream, Validator validator, Map<String, ?> map) throws IOException, CsvValidationException, LocalValidationException {
-    List<? extends CsvBase<?>> csvObjs = readToCsv(targetClass, inputStream, validator);
-    return csvObjs.stream().map(csvObj -> {
-      try {
-        return ((CsvBase<?>) csvObj.extractMotherEntity(validator, map)).extractMotherEntity(validator, map);
-      } catch (LocalValidationException e) {
-        throw new RuntimeException(e);
+  private <T> void validateCsvObjects(List<T> csvObjects, List<String> errorMessages) {
+    for (int i = 0; i < csvObjects.size(); i++) {
+      Set<ConstraintViolation<T>> violations = validator.validate(csvObjects.get(i));
+      if (!violations.isEmpty()) {
+        for (ConstraintViolation<T> violation : violations) {
+          String errorMessage = String.format(
+            "Line %d: Property '%s' failed validation: %s",
+            2 + i,
+            violation.getPropertyPath(),
+            violation.getMessage()
+          );
+          errorMessages.add(errorMessage);
+        }
       }
-    }).toList();
-  }
-
-  public <T> List<T> read(Class<T> clazz, InputStream inputStream) throws IOException {
-    CsvSchema schema = csvMapper.schemaFor(clazz).withHeader();
-    MappingIterator<T> mappingIterator = csvMapper.readerFor(clazz).with(schema).readValues(inputStream);
-    return mappingIterator.readAll();
+    }
   }
 }
